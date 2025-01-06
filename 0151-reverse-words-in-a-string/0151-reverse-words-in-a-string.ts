8a26dce1ab0c8764a388a14302638b6c07334b91
Rahul <iamrahultambe@gmail.com>
2025-01-06 21:04:25 +0530

    Time: 1 ms (82.56%), Space: 51.9 MB (41.17%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
@@ -1,17 +1,17 @@
 function reverseWords(s: string): string {
     let reverseWords = ''
     let word = ''
+    let space = ''
     for(let i=0; i<s.length; i++){
         if(s[i]==' ' && word.length){
-            let space = reverseWords.length ? ' ' : ''
             reverseWords = word + space + reverseWords;
             word = ''
+            space = ' '
         }else if(s[i]!=' '){
             word+=s[i]
         }
     }
     if(word.length){
-        let space = reverseWords.length ? ' ' : ''
         reverseWords = word + space + reverseWords;
     }
     return reverseWords
