aeb9df56208c893cb5f37fdc125d8af8dbde3d2b
Rahul <iamrahultambe@gmail.com>
2025-01-08 04:15:08 +0530

    Time: 3 ms (64.39%), Space: 69.5 MB (72.2%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.ts
@@ -0,0 +1,14 @@
+function increasingTriplet(nums: number[]): boolean {
+    let firstNum = Infinity
+    let secondNum = Infinity
+    for (const num of nums){
+        if(num <= firstNum){
+            firstNum = num
+        }else if(num <= secondNum){
+            secondNum = num
+        }else{
+            return true
+        }
+    }
+    return false
+};
